8e23fcdb2d80a830b9db6d761d839af607ecab27
Kodirjon Norov <kodir@yahoo.com>
2026-04-10 00:32:37 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,8 @@ scope = [
     "https://www.googleapis.com/auth/drive"
 ]
 creds_json = os.getenv("GOOGLE_CREDENTIALS")
+print("DEBUG CREDS:", creds_json[:50] if creds_json else "EMPTY")
+
 creds_dict = json.loads(creds_json.replace("\\n", "\n"))
 
 creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
